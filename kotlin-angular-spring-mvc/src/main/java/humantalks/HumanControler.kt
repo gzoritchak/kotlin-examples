cc9d5ea97e93807b2da6f3537ce9b89d72b840d0
Gaetan Zoritchak <g.zoritchak@gmail.com>
2013-12-08 00:11:19 +0100

    Minor refactoring: - using a constant=150 as the max age.

--- kotlin-angular-spring-mvc/src/main/java/humantalks/HumanControler.kt
+++ kotlin-angular-spring-mvc/src/main/java/humantalks/HumanControler.kt
@@ -9,13 +9,15 @@ public class HumanController() {
 
     Autowired var humanRepo:HumanRepo? = null
 
+    val MAX_AGE = 150
+
     RequestMapping(value=array("/humans"))
     ResponseBody
-    fun listHumans(RequestParam(required=false, defaultValue="200") ageMax:Int)=
+    fun listHumans(RequestParam(required=false) ageMax:Int?)=
             humanRepo!!
                 .listHumans()
-                .filter { it.age < ageMax}
-                .sortBy { it.age }
-                .reverse()
+                    .filter { it.age < ageMax ?: MAX_AGE}
+                    .sortBy { it.age }
+                    .reverse()
 
 }
